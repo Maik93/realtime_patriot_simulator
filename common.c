da875618fe512b36207cc7cab06dc60c2b8522bd
Maik Mugnai <super.mugno@gmail.com>
2017-07-26 02:49:08 +0200

    graphic divided in static part and dynamically-evaluated part

--- common.c
+++ common.c
@@ -19,6 +19,9 @@
 #include "trackers.h"
 #include "rocket_laucher.h"
 
+// Static part of graphics, in which we draw each time.
+BITMAP	*screen_base;
+
 int world2abs_x(int x) {
 	return WORLD_BOX_X1 + x;
 }
@@ -88,38 +91,50 @@ void right_menu() {
 	     TRACK_D2_X + TRACKER_RES * TRACK_DSCALE / 2, TRACK_D2_Y + TRACKER_RES * TRACK_DSCALE / 2, BORDER_COL);
 	rect(screen_buff, TRACK_D3_X - TRACKER_RES * TRACK_DSCALE / 2, TRACK_D3_Y - TRACKER_RES * TRACK_DSCALE / 2,
 	     TRACK_D3_X + TRACKER_RES * TRACK_DSCALE / 2, TRACK_D3_Y + TRACKER_RES * TRACK_DSCALE / 2, BORDER_COL);
-
-	// tracker views on right side of the screen
-	for (i = TRACKER_BASE_INDEX; i < TRACKER_TOP_INDEX; i++)
-		if (tp[i].index != -1)
-			tracker_display(i - TRACKER_BASE_INDEX);
-
-	// TODO: handle deadlines
-	/*if (deadline_miss(a))
-		show_dmiss(a);*/
 }
 
-// Graphical task.
+// Graphical task. Before the main loop, we can compose basic window background,
+// then we can draw on it each time, insead of drawing again everything.
 void *graphic_task(void* arg) {
 	int i, a;
 	float delta_t = TSCALE * (float)20 / 1000;
 
+	// initializing screen bitmaps
+	screen_base = create_bitmap(SCREEN_W, SCREEN_H);
+	clear_bitmap(screen_base);
+	clear_to_color(screen_buff, GND);
+
+	draw_world();
+	draw_radar_symbol();
+
+	// DBG: dots to test scanner
+	/*circlefill(screen_buff, 343, 319, 10, RED);
+	circlefill(screen_buff, 338, 404, 10, GREEN);
+	circlefill(screen_buff, 253, 319, 10, BORDER_COL);
+	circlefill(screen_buff, 190, 404, 10, WHITE);*/
+
+	top_menu();
+	right_menu();
+
+	// store this basic screen_buff
+	blit(screen_buff, screen_base, 0, 0, 0, 0, screen_buff->w, screen_buff->h);
+
+	// update screen with the content of screen_buff
+	blit(screen_buff, screen, 0, 0, 0, 0, screen_buff->w, screen_buff->h);
+
+	// starts radar_task and rocket_laucher_task
+	start_task(radar_task, 2, 2, 30, RADAR_INDEX);
+	start_task(rocket_laucher_task, 50, 50, 50, ROCKET_LAUCHER_INDEX);
+
 	a = get_task_index(arg);
 	set_period(a);
 	while (!sigterm_tasks) {
-		clear_to_color(screen_buff, GND);
 
-		draw_world();
-		draw_radar_symbol();
+		// recover basic screen_buff
+		blit(screen_base, screen_buff, 0, 0, 0, 0, screen_buff->w, screen_buff->h);
 
 		draw_launcher();
 		draw_current_trajectory();
-
-		// DBG: dots to test scanner
-		/*circlefill(screen_buff, 343, 319, 10, RED);
-		circlefill(screen_buff, 338, 404, 10, GREEN);
-		circlefill(screen_buff, 253, 319, 10, BORDER_COL);
-		circlefill(screen_buff, 190, 404, 10, WHITE);*/
 
 		// enemy and Patriot missiles
 		for (i = ENEMY_MISSILES_BASE_INDEX; i < PATRIOT_MISSILES_TOP_INDEX; i++) {
@@ -136,21 +151,22 @@ void *graphic_task(void* arg) {
 			if (tp[i].index != -1)
 				draw_predictions(i, delta_t / 3);
 
-		top_menu();
 		draw_radar_display();
-		right_menu();
+
+		// tracker views on right side of the screen
+		for (i = TRACKER_BASE_INDEX; i < TRACKER_TOP_INDEX; i++)
+			if (tp[i].index != -1)
+				tracker_display(i - TRACKER_BASE_INDEX);
+
+		// TODO: handle deadlines
+		/*if (deadline_miss(a))
+			show_dmiss(a);*/
 
 		// this will hide a green dot in (0, 0) caused by trails
 		putpixel(screen_buff, WORLD_BOX_X1, WORLD_BOX_Y2, BKG);
 
 		// update screen with the content of screen_buff
 		blit(screen_buff, screen, 0, 0, 0, 0, screen_buff->w, screen_buff->h);
-
-		// if not already running, starts radar_task and rocket_laucher_task
-		if (tp[RADAR_INDEX].index == -1)
-			start_task(radar_task, 2, 2, 30, RADAR_INDEX);
-		if (tp[ROCKET_LAUCHER_INDEX].index == -1)
-			start_task(rocket_laucher_task, 50, 50, 50, ROCKET_LAUCHER_INDEX);
 
 		wait_for_period(a);
 	}
